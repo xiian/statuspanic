fa46655bc27eca1dd821c8b9552b6b783b42f88b
Tom Sartain <tomsartain@gmail.com>
2011-11-13 00:58:08 -0500

    Refactor main to nest require calls

--- webroot/c/js/main.js
+++ webroot/c/js/main.js
@@ -1,8 +1,10 @@
-require(['models/board','views/board', 'lib/jquery','lib/underscore','lib/backbone'], function(Board, Board_View){
-    $.getJSON('/config.json', function(data){
-        new Board_View({
-            model: new Board(data),
-            el: $('body')
-        }).render();
+require(['lib/jquery','lib/underscore','lib/backbone'], function(){
+    require(['models/board','views/board'],function(Board, Board_View){
+        $.getJSON('/config.json', function(data){
+            new Board_View({
+                model: new Board(data),
+                el: $('body')
+            }).render();
+        });
     });
 });

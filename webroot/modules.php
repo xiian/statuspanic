9ea78f7e8eec79cec9a5e61d2034c02e8c026627
Tom Sartain <tomsartain@gmail.com>
2011-11-14 11:31:57 -0500

    Fix for lack of SCRIPT_URL

--- webroot/modules.php
+++ webroot/modules.php
@@ -10,7 +10,8 @@ spl_autoload_register(function($class){
 
 // Hacky way to get to this
 if (substr($_SERVER['REQUEST_URI'], 0, 8) == '/modules') {
-    require('../application' . $_SERVER['SCRIPT_URL']);
+    $script = parse_url($_SERVER['REQUEST_URI']);
+    require('../application' . $script['path']);
 } else {
     header('HTTP/1.1 404 Not Found');
 }

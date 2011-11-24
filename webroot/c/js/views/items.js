979d0481edb20752db688a9b4d65f87f6e3dbbc7
Tom Sartain <tomsartain@gmail.com>
2011-11-23 20:45:25 -0500

    Make the items load in the defined order no matter how long the specific item takes to render.

--- webroot/c/js/views/items.js
+++ webroot/c/js/views/items.js
@@ -1,11 +1,13 @@
 define(['models/item'], function(Item){
     var View = Backbone.View.extend({
         addOne: function(item) {
+          var placeholder = $('<div></div>');
+          this.el.append(placeholder);
             require(['views/item/' + item.get('name')], _.bind(function(Item_View){
                 var element = new Item_View({
                     'model': item
                 }).render().el;
-                this.el.append(element);
+                placeholder.replaceWith(element);
             }, this));
         },
 

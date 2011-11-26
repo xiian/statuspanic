e9f0b574f190210c21c7039749a6d14705b4eb5d
Tom Sartain <tomsartain@gmail.com>
2011-11-26 18:24:43 -0500

    DEBUG: Add cachebuster to require.js

--- webroot/c/js/main.js
+++ webroot/c/js/main.js
@@ -1,3 +1,9 @@
+var oldA = require.attach;
+require.attach = function (url, cName, mName, cb, type) {
+  url += (url.indexOf('?') === -1 ? '?' : '&') + 'bust=' + (new Date().getTime());
+  return oldA.call(require, url, cName, mName, cb, type);
+};
+
 require(['lib/jquery','lib/underscore','lib/backbone'], function(){
     require(['models/board','views/board'],function(Board, Board_View){
         $.getJSON('/config.json', function(data){

2127eb3562a21904c2d33751bc84799e08faa0ec
Tom Sartain <tomsartain@gmail.com>
2011-12-10 17:37:54 -0500

    Switch to having grid being overview of roadmap

--- application/modules/grid.module.php
+++ application/modules/grid.module.php
@@ -1,35 +1,62 @@
 <?php
-function makeGravatar($email, $title = '') {
-    if (empty($title)) {
-        $title = $email;
-    }
-    return'<img src="http://www.gravatar.com/avatar.php?gravatar_id='. md5(trim($email)) .'&s=40&d=monsterid" title="' . $title . '"> ';
+/* DATA */
+function makeGraph($inDev, $devComp, $qa, $validate) {
+  $x = '';
+  $levels = array();
+  $levels[] = array(
+    'label' => 'in development',
+    'class' => 'indev',
+    'count' => $inDev
+    );
+  $levels[] = array(
+    'label' => 'dev complete',
+    'class' => 'devcomp',
+    'count' => $devComp
+    );
+  $levels[] = array(
+    'label' => 'in QA',
+    'class' => 'inqa',
+    'count' => $qa
+    );
+  $levels[] = array(
+    'label' => 'awaiting validation',
+    'class' => 'validation',
+    'count' => $validate
+    );
+
+  $total = $inDev + $devComp + $qa + $validate;
+
+  foreach($levels as $level) {
+    $width = ($level['count'] / $total) * 100;
+    $x .= '<span class="' . $level['class'] . '" style="width: ' . $width . '%;" title="' . $level['count'] . ' tickets ' . $level['label'] . '">&nbsp;</span>';
+  }
+  return '<div class="statusbar">' . $x . '</div>';
 }
 
-/* DATA */
 function getData() {
-    define('OFFICE', 'In Office');
-    define('REMOTE', 'Remote');
-    define('LAZY'  , 'Lazy');
-    $users = array();
+  $releases = array();
 
-    $users['toms@kindlingapp.com']      = OFFICE;
-    $users['garretk@kindlingapp.com']   = OFFICE;
-    $users['josephl@kindlingapp.com']   = OFFICE;
-    $users['tim@kindlingapp.com']       = LAZY;
-    $users['patrickf@kindlingapp.com']  = REMOTE;
-    $users['dan.summa@kindlingapp.com'] = OFFICE;
+  $release = array(
+    'product' => 'Kindling',
+    'version' => '2.3',
+    'date'    => 'Fri Dec 11',
+    'graph'   => makeGraph(rand(1,6), rand(0,3), rand(5,9), rand(2, 20))
+    );
+  $releases[] = $release;
 
-    $locations = array();
-    foreach($users as $user => $where) {
-        $gravatar = makeGravatar($user);
-        $locations[$where][] = $gravatar;
-    }
+  $release = array(
+    'product' => 'Bonfire',
+    'version' => '1.5.10',
+    'date'    => 'Mon Dec 02',
+    'graph'   => makeGraph(rand(1,6), rand(0,3), rand(5,9), rand(2, 20))
+    );
+  $releases[] = $release;
 
-    foreach($locations as $where => $users) {
-        $data[] = array($where, implode($users, ''));
-    }
-    return $data;
+  $data = array();
+  foreach($releases as $release) {
+    $data[] = array($release['product'], $release['version'], $release['date'], $release['graph']);
+  }
+  return $data;
 }
 
 /* DISPLAY */

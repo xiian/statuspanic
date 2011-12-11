f5f41323b78a99b030ae5a57075d4a3ec6997ce2
Tom Sartain <tomsartain@gmail.com>
2011-12-11 00:14:02 -0500

    Build file for using requirejs optimizer to create main-built.js

---
+++ webroot/c/js/build.js
@@ -0,0 +1,8 @@
+({
+  "baseUrl": ".",
+  "out": "main-built.js",
+  "name": "main",
+  // Make sure the views are included, since they are conditionally loaded
+  "include": ['views/item/bargraph', 'views/item/clock', 'views/item/countdown', 'views/item/grid', 'views/item/space', 'views/item/weather']
+}
+)

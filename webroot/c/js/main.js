e3c929c24730cc4fa59354b4ec790ded649fe432
Tom Sartain <tomsartain@gmail.com>
2011-12-10 20:53:50 -0500

    Put basic error checking in config loading

--- webroot/c/js/main.js
+++ webroot/c/js/main.js
@@ -6,11 +6,18 @@ require.attach = function (url, cName, mName, cb, type) {
 
 require(['lib/jquery','lib/underscore','lib/backbone'], function(){
     require(['models/board','views/board'],function(Board, Board_View){
-        $.getJSON('/config.json', function(data){
-            new Board_View({
-                model: new Board(data),
-                el: $('body')
-            }).render();
+        $.ajax({
+          url: '/config.json',
+          dataType: 'json',
+          success: function(data){
+              new Board_View({
+                  model: new Board(data),
+                  el: $('body')
+              }).render();
+          },
+          error: function(xhr, status, e){
+            alert('There was an error loading the config file. Error was: ' + e);
+          }
         });
     });
 });

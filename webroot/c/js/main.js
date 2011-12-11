16632158f64c7c570e11821d83418e964967b16d
Tom Sartain <tomsartain@gmail.com>
2011-12-10 21:28:12 -0500

    Use priority array to make sure that jquery and underscore are loaded before backbone and the rest

--- webroot/c/js/main.js
+++ webroot/c/js/main.js
@@ -3,7 +3,12 @@ require.attach = function (url, cName, mName, cb, type) {
   url += (url.indexOf('?') === -1 ? '?' : '&') + 'bust=' + (new Date().getTime());
   return oldA.call(require, url, cName, mName, cb, type);
 };
-
+require.config({
+  priority: [
+    'lib/jquery',
+    'lib/underscore'
+  ]
+});
 require(['lib/jquery','lib/underscore','lib/backbone'], function(){
     require(['models/board','views/board'],function(Board, Board_View){
         $.ajax({
